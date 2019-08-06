31bab52e32e8d7ab26be4e995e83dd34b986e342
Barbara Peric <b.peric10@gmail.com>
2019-08-06 14:00:49 +0200

    task manager done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,38 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
+    # @task = Task.find(params[:id].to_i)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.create(task_strong_params)
+    redirect_to task_path(task)
+  end
+
+  def destroy
+    # @task = Task.find(params[:id].to_i)
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_strong_params
+    params.require(:task).permit(:title, :description, :completed)
+  end
+
+  def set_task
     @task = Task.find(params[:id].to_i)
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
 
 get '/tasks', to: 'tasks#index'
 # get '/tasks', to:
+get '/tasks/new', to: 'tasks#new'
+post '/tasks', to: 'tasks#create'
+
+
 get '/tasks/:id', to: 'tasks#show', as: :task
+
+delete '/tasks/:id', to: 'tasks#destroy', as: :destroy_task
 
 end
